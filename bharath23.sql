1539babbca929bc5ef44b4284cc5dec2f55d817b
laxmi243 <lakshmiprasadbodigutta@gmail.com>
2025-01-14 21:41:54 +0530

    Add files via upload

---
+++ bharath23.sql
@@ -0,0 +1,16 @@
+create table student(id int,name varchar(90),address varchar(89),phone_no varchar(10));
+
+insert into student(id,name,address,phone_no)
+values             (8,'Bharath Kumar','Mangalampeta','9701445805'),
+                   (9,'Dileep','Palamaneru','7685246912'),
+				   (10,'Lachu','Chittoor','8541236985'),
+				   (11,'Raju Hemanth','Irala','7671009268'),
+				   (12,'Hari Prasad','Tirupati','8977789319');
+
+				  
+
+select * from student;
+
+
+				  
+
